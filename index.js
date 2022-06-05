e196b1405af0656683a2689e1ab6429079c46244
barkahharis <barkahris@gmail.com>
2022-06-05 21:44:59 +0700

    callback function

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, sort) => {
+  const sortArray = sort(data);
+  const tempArray = [];
+
+  for (let i = 0; i < sortArray.length; i++) {
+    tempArray[i] = `${i + 1}. ${sortArray[i]}`;
+  }
+
+  return tempArray;
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => data.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) =>  data.reverse();
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
